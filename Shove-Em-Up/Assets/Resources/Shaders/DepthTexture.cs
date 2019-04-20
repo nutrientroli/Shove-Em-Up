cb226a21ee847db153cc42fed05b9a0496654d78
TheMaker1993 <danithemaker@gmail.com>
2019-04-20 12:20:10 +0200

    [DANIEL] Introduccion de postropresado y introduccion barril para pruebas

---
+++ Shove-Em-Up/Assets/Resources/Shaders/DepthTexture.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class DepthTexture : MonoBehaviour
+{
+
+    private Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        if (cam)
+            cam.depthTextureMode = DepthTextureMode.Depth;
+        else
+            Debug.LogError("No camera component found on the Game object");
+    }
+
+}
