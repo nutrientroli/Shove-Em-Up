f0a88eb7ec4ad09e18cb0aef456d543156433ab3
SerTor2 <sertor27@gmail.com>
2019-05-02 00:20:18 +0200

    habilidad del platano al fin

--- Shove-Em-Up/Assets/Scripts/Players/Hability/BananaHabilityScript.cs
+++ Shove-Em-Up/Assets/Scripts/Players/Hability/BananaHabilityScript.cs
@@ -16,15 +16,17 @@ public class BananaHabilityScript : HabilityScript
     public override void UseHability()
     {
         base.UseHability();
-        for (int i = 0; i < 8; i++)
+        for (int i = 0; i < 3; i++)
         {
             bananas.Add(Instantiate(bananaPrefab, transform.position, bananaPrefab.transform.rotation));
             bananas[bananas.Count - 1].GetComponent<BananaScript>().SetMyPlayer(gameObject);
             Vector3 forward;
             float angle = 45;
-            forward = new Vector3(Mathf.Cos(Mathf.PI * 2 * (i - 1) / 360 * angle),0, Mathf.Sin(Mathf.PI * 2 * (i - 1) / 360 * angle));
+            float myAngle = Quaternion.ToEulerAngles(gameObject.transform.rotation).y * 57.15f;
+            forward = new Vector3(Mathf.Cos(Mathf.PI * 2 * (i - 1) / 360 * angle + Mathf.PI/2),0, Mathf.Sin(Mathf.PI * 2 * (i - 1) / 360 * angle + Mathf.PI / 2));
+            forward = Quaternion.AngleAxis(myAngle, Vector3.up) * forward;
             bananas[bananas.Count - 1].GetComponent<BananaScript>().SetForward((forward).normalized);
-            bananas[bananas.Count - 1].GetComponent<BananaScript>().SetSpeed(15);
+            bananas[bananas.Count - 1].GetComponent<BananaScript>().SetSpeed(20);
         }
     }
 
