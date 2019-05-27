3f2a458bc2c08b30e71ced0d50e96ffdc5524a92
nutrientroli <garcia_raul_alcazar@hotmail.com>
2019-05-27 19:34:30 +0200

    [Raul] Score Screen

---
+++ Shove-Em-Up/Assets/Scripts/UI/GifScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GifScript : MonoBehaviour
+{
+    public Image gif;
+    public List<Sprite> frames = new List<Sprite>();
+    private int frame = 0;
+    private int framesPerSecond = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Play();
+    }
+
+    private void Play() {
+        frame  = (int)(Time.time* framesPerSecond) % frames.Count;
+        gif.sprite = frames[frame];
+    }
+}
---
+++ Shove-Em-Up/Assets/Scripts/UI/ProgressBarScript.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarScript : MonoBehaviour
+{
+    public int player;
+    public Color color;
+    public Image bar;
+    [SerializeField] private float speed = 0.2f;
+    private bool runAnimation = true;
+    [SerializeField] private float maxvalue = 50;
+    [SerializeField] private float maxvalueglobal = 100;
+    private float lerpvalue = 0;
+
+    void Start() {
+        maxvalue = ScoreManager.GetInstance().GetPoints(player);
+        maxvalueglobal = ScoreManager.GetInstance().GetMaxPoints();
+        bar.color = color;
+        SetProgress(0);
+    }
+
+    void Update() {
+        if (runAnimation) {
+            lerpvalue += speed * Time.deltaTime;
+            SetProgress(GetPercent(Mathf.Lerp(0, maxvalueglobal, lerpvalue)));
+        }
+    }
+
+    private float GetPercent(float _value) {
+        if (_value >= maxvalue) {
+            _value = maxvalue;
+            runAnimation = false;
+        }
+        return  _value / maxvalueglobal;
+    }
+
+    private void SetProgress(float _percent) {
+        if (_percent >= 1)   _percent = 1;
+        bar.fillAmount = _percent;
+    }
+}
