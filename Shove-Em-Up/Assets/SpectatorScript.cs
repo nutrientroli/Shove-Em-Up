c69f09f165e90bb2646cee314c63749aec222b70
nutrientroli <garcia_raul_alcazar@hotmail.com>
2019-05-29 16:41:03 +0200

    [RAUL] Spectator

---
+++ Shove-Em-Up/Assets/SpectatorScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpectatorScript : MonoBehaviour
+{
+    public float type = 0;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start() {
+        anim.SetFloat("Type", type);
+    }
+
+}
