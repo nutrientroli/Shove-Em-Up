51200f512e6bcb3bd25e550078d8ad6d254f70f9
nutrientroli <garcia_raul_alcazar@hotmail.com>
2019-05-30 16:09:23 +0200

    [RAUL] Spectator random animation

--- Shove-Em-Up/Assets/Scripts/Utils/SpectatorScript.cs
+++ Shove-Em-Up/Assets/Scripts/Utils/SpectatorScript.cs
@@ -10,7 +10,7 @@ public class SpectatorScript : MonoBehaviour
 
     // Start is called before the first frame update
     void Start() {
-        type = Random.Range(0, 1);
+        type = Random.Range(0.0f, 1.0f);
         anim.SetFloat("Type", type);
     }
 
