8df2a4a8941f39fe317072f673ae8229c7a3c446
TheMaker1993 <danithemaker@gmail.com>
2019-06-15 18:04:09 +0200

    [DANIEL] Update luces y colliders

---
+++ Shove-Em-Up/Assets/Res/Scripts/Enviroment/LightCollorChangerController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controlador que cambia el color de la luz emitida ademas del material que este usa
+/// </summary>
+public class LightCollorChangerController : MonoBehaviour
+{
+
+    private Material lightMaterial;
+    private Light lightToManage;
+
+    [SerializeField] private Color standardStateColor;
+    [SerializeField] private Color triggerStateColor;
+
+    public void SetStandardState ()
+    {
+        SetLightColor(standardStateColor);
+    }
+    public void SetTriggerState ()
+    {
+        SetLightColor(triggerStateColor);
+    }
+
+    public void SetLightColor(Color _colorToSet)
+    {
+        // light
+        lightToManage.color = _colorToSet;
+
+        // material
+        lightMaterial.SetColor("Emision", _colorToSet);
+
+    }
+ 
+
+
+    private void Awake()
+    {
+        lightToManage = GetComponentInChildren<Light>();
+        lightMaterial = GetComponentInChildren<Renderer>().material;
+
+    }
+
+
+    /*
+    private IEnumerator FadeIn(float _time)
+    {
+        float increasePerSecond = (maxLightIntensity - currentBrightness) / _time;
+        while (currentBrightness < maxLightIntensity)
+        {
+            lightToManage.intensity += increasePerSecond;
+            currentBrightness = lightToManage.intensity;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+    }
+
+    private IEnumerator FadeOut(float _time)
+    {
+        float decreasePerSecond = (currentBrightness - minLightIntensity) / _time;
+        while (currentBrightness > minLightIntensity)
+        {
+            lightToManage.intensity -= decreasePerSecond;
+            currentBrightness = lightToManage.intensity;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+    }
+    */
+}
