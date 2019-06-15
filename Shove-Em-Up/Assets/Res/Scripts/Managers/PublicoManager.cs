1bcf1574b569f1fe54b8a5fb652a00e7f83d853c
TheMaker1993 <38749147+TheMaker1993@users.noreply.github.com>
2019-06-15 12:31:53 +0200

    cambios de animaciones del publico

---
+++ Shove-Em-Up/Assets/Res/Scripts/Managers/PublicoManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PublicoManager
+{
+    #region Singleton Pattern
+    private static PublicoManager instance;
+    private PublicoManager() { }
+    public static PublicoManager GetInstance()
+    {
+        if (instance == null) instance = new PublicoManager();
+        return instance;
+    }
+    #endregion
+
+    private List<SpectatorScript> spectators = new List<SpectatorScript>();
+
+    public void AddToList(SpectatorScript _spectator)
+    {
+        spectators.Add(_spectator);
+    }
+
+    public void ChangeAnimations()
+    {
+        foreach(SpectatorScript sp in spectators)
+        {
+            if (Random.Range(0, 5) <= 1)
+                sp.ChangeAnimation();
+        }
+    }
+}
