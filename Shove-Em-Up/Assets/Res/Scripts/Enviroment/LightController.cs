32a794b8b8e2177258c209d33a82bebf7528bd6a
TheMaker1993 <danithemaker@gmail.com>
2019-06-15 12:36:32 +0200

    [DANIEL] Update light controller

---
+++ Shove-Em-Up/Assets/Res/Scripts/Enviroment/LightController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    // references
+    [SerializeField] private Light lightToManage;
+
+    private float currentBrightness;
+    [SerializeField] private float minLightIntensity, maxLightIntensity;
+
+    [SerializeField] private bool useCurrentAsMaxIntensity = true;
+
+
+
+    public void EnableLight(float _time)
+    {
+        StartCoroutine(FadeIn(_time));
+    }
+    public void DisableLight(float _time)
+    {
+        StartCoroutine(FadeOut(_time));
+    }
+
+
+    private void Awake()
+    {
+
+        lightToManage = GetComponentInChildren<Light>();
+
+        currentBrightness = lightToManage.intensity;
+
+        if (useCurrentAsMaxIntensity)
+            maxLightIntensity = currentBrightness;
+
+        if (minLightIntensity == maxLightIntensity)
+            Debug.LogError("LIGHTS_: El valor de emisividad minimo y maximo no pueden ser el mismo");
+
+    }
+
+    //private void Update()
+    //{
+    //    if (Input.GetKeyUp(KeyCode.Space))
+    //    {
+    //        if (currentBrightness == minLightIntensity)
+    //            EnableLight(5f);
+    //        else if (currentBrightness == maxLightIntensity)
+    //            DisableLight(5f);
+    //    }
+    //}
+
+    private IEnumerator FadeIn(float _time)
+    {
+        float increasePerSecond = (maxLightIntensity - currentBrightness) / _time;
+        while (currentBrightness < maxLightIntensity)
+        {
+            lightToManage.intensity += increasePerSecond;
+            currentBrightness = lightToManage.intensity;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+    }
+
+    private IEnumerator FadeOut (float _time)
+    {
+        float decreasePerSecond = (currentBrightness - minLightIntensity) / _time;
+        while (currentBrightness > minLightIntensity)
+        {
+            Debug.Log(decreasePerSecond);
+
+            lightToManage.intensity -= decreasePerSecond;
+            currentBrightness = lightToManage.intensity;
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+    }
+
+
+}
