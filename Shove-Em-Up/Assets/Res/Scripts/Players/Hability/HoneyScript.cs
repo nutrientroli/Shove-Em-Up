74a37ec98fa2fc72da4938a3a960e9d7b081d0e6
SerTor2 <sertor27@gmail.com>
2019-06-16 16:08:33 +0200

    sin problemas

--- Shove-Em-Up/Assets/Res/Scripts/Players/Hability/HoneyScript.cs
+++ Shove-Em-Up/Assets/Res/Scripts/Players/Hability/HoneyScript.cs
@@ -11,7 +11,6 @@ public class HoneyScript : MonoBehaviour
     private float currentTime = 0;
     private Vector3 firstScale;
     private GameObject myPlayer;
-    private bool gravity = true;
     public LayerMask layerMask;
     private SphereCollider collider;
 
@@ -26,28 +25,7 @@ public class HoneyScript : MonoBehaviour
         gameObject.transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
         gameObject.transform.localScale -= Time.deltaTime * Vector3.one * 1.2f;
 
-        RaycastHit rayHit;
-        if (Physics.Raycast(gameObject.transform.position, Vector3.down, out rayHit, collider.radius * 2f, layerMask))
-        {
-            gravity = false;
-        }
-        else
-        {
-            if (Physics.Raycast(gameObject.transform.position + Vector3.right, Vector3.down, out rayHit, collider.radius * 1.5f, layerMask))
-            {
-                gravity = false;
-            }
-            else if (Physics.Raycast(gameObject.transform.position + Vector3.left, Vector3.down, out rayHit, collider.radius * 1.5f, layerMask))
-            {
-                gravity = false;
-            }
-            else gravity = true;
-        }
-
-        if (gravity)
-            gameObject.transform.position += forward * Time.deltaTime * speed + Vector3.down * speed * Time.deltaTime / 5;
-        else
-            gameObject.transform.position += forward * Time.deltaTime * speed;
+        gameObject.transform.position += forward * Time.deltaTime * speed + Vector3.down * speed * Time.deltaTime / 5;
 
         if (gameObject.transform.localScale.x <= 0.5f)
             gameObject.SetActive(false);
